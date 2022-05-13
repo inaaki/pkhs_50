56f69de86d0b0bb509e589c342b98ce38fbfcbc2
Inzamul Haque <inzamul119@gmail.com>
2022-05-14 00:27:20 +0600

    feature: add svg background to hero component

--- src/Theme/components/Heading.js
+++ src/Theme/components/Heading.js
@@ -2,7 +2,7 @@ const Heading = {
   variants: {
     'hero-heading': {
       color: 'brand.500',
-      fontSize: ['4xl', '5xl', '4xl', '5xl','6xl'],
+      fontSize: ['4xl', '5xl', '4xl', '5xl', '6xl'],
       lineHeight: 1.1,
     },
   },
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,54 +9,85 @@ import {
   SimpleGrid,
   Square,
   Text,
-  useBreakpointValue,
+  useMediaQuery,
   VStack,
 } from '@chakra-ui/react';
 import React from 'react';
 import school_1 from '../assets/images/school_1.jpeg';
 import school_2 from '../assets/images/school_2.jpeg';
+import hero_bg from '../assets/svg/hero_bg.svg';
 
 function Hero() {
-  const gridColumns = useBreakpointValue({ base: 1, md: 2 });
+  const sectionCss = {
+    bgImage: `url(${hero_bg})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: {
+      base: 'auto 410px',
+      sm: 'auto 450px',
+      md: 'cover',
+    },
+    backgroundPosition: {
+      base: 'bottom -1% center',
+      md: 'center right 90%',
+    },
+  };
 
   return (
-    <Container maxW={'container.2xl'} p="10" pt={{ base: '32', md: '10' }}>
-      <SimpleGrid minH={'90vh'} columns={gridColumns} alignItems="center">
-        <VStack
-          spacing="6"
-          align={{ base: 'center', md: 'start' }}
-          ps={{ base: 0, md: '14' }}
+    <chakra.section sx={sectionCss} mt="66.5px">
+      <Container maxW={'container.2xl'} p="10">
+        <SimpleGrid
+          alignItems="center"
+          columns={{ base: 1, md: 2 }}
+          minH={{ base: 'auto', '2xl': '36.5vw' }}
+          rowGap={10}
         >
-          <Box textAlign={{ base: 'center', md: 'initial' }} maxW={'md'}>
-            <Heading as={'h1'} variant={'hero-heading'} ms="-0.5">
-              Celebrating
-              <chakra.span color={'gray.600'}> 50 Years </chakra.span>
-              of lovely connections
-            </Heading>
-            <Text mt={4} lineHeight="1.5" pe="2">
-              We are united to build a memory. You're honourably invited to join
-              us We are united to build a memory. You're honourably invited to
-              join us We are united to build a memory.
-            </Text>
-          </Box>
-          <Button
-            variant="cta"
-            size="cta"
-            onClick={() => console.log('routing to register page')}
+          <VStack
+            spacing="6"
+            align={{ base: 'center', md: 'start' }}
+            ps={{ base: 0, md: '14' }}
           >
-            Register Now
-          </Button>
-        </VStack>
-        {/* hero image */}
-        <Center>
-          <HeroImage />
-        </Center>
-      </SimpleGrid>
-    </Container>
+            <Box
+              textAlign={{ base: 'center', md: 'initial' }}
+              maxW={{
+                base: 'md',
+                md: 'xs',
+                lg: 'sm',
+                xl: 'md',
+              }}
+            >
+              <Heading as={'h1'} variant={'hero-heading'} ms="-0.5">
+                Celebrating
+                <chakra.span color={'gray.600'}> 50 Years </chakra.span>
+                of lovely connections
+              </Heading>
+              <Text mt={4} lineHeight="1.5" pe="2">
+                We are united to build a memory. You're honourably invited to
+                join us We are united to build a memory. You're honourably
+                invited to join us We are united to build a memory.
+              </Text>
+            </Box>
+            <Button
+              variant="cta"
+              size="cta"
+              onClick={() => console.log('routing to register page')}
+            >
+              Register Now
+            </Button>
+          </VStack>
+          {/* hero image */}
+          <Center>
+            <HeroImage />
+          </Center>
+        </SimpleGrid>
+      </Container>
+    </chakra.section>
   );
 }
 
 function HeroImage() {
+  const [isMobile] = useMediaQuery('(hover:none)');
+  console.log(isMobile);
+
   return (
     <Square
       boxSize={['350px', '400px', '400px', '500px', '600px']}
@@ -64,7 +95,7 @@ function HeroImage() {
       data-group
     >
       <ImageCard
-        _groupHover={{ left: '25%' }}
+        _groupHover={!isMobile ? { left: '25%' } : ''}
         left="28.5%"
         pos="absolute"
         src={school_1}
@@ -74,7 +105,7 @@ function HeroImage() {
         width={{ base: '70%', sm: '75%', lg: '65%' }}
       />
       <ImageCard
-        _groupHover={{ left: '10%' }}
+        _groupHover={!isMobile ? { left: '10%' } : ''}
         left="6.5%"
         pos="absolute"
         src={school_2}
@@ -101,13 +132,14 @@ function ImageCard({
   ...props
 }) {
   const baseStyle = {
-    border: '1px',
-    borderColor: 'gray.200',
     borderRadius: 'md',
+    border: '1px',
+    borderColor: 'blackAlpha.300',
     boxShadow: 'lg',
     cursor: cursor || 'pointer',
     overflow: overflow || 'hidden',
     p: '2',
+    backgroundColor: 'gray.50',
     transition: transition || 'all 250ms cubic-bezier(0.4, 0, 0.2, 1) 50ms',
   };
 
@@ -122,7 +154,7 @@ function ImageCard({
 
   return (
     <Box sx={baseStyle} _hover={hoverStyle} _active={activeStyle} {...props}>
-      <Image borderRadius={baseStyle.md} src={src} srcSet={srcset} />
+      <Image borderRadius={baseStyle.borderRadius} src={src} srcSet={srcset} />
     </Box>
   );
 }
