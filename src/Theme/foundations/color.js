3352b65d9d628fa3c79517f9d3f25c6ee9018d0e
Inzamul Haque <inzamul119@gmail.com>
2022-05-19 19:04:04 +0600

    add highlight color for text

--- src/Theme/foundations/color.js
+++ src/Theme/foundations/color.js
@@ -2,12 +2,14 @@ import { theme } from '@chakra-ui/react';
 
 const heading = theme.colors.gray['600'];
 const text = theme.colors.gray['500'];
+const textHighlight = theme.colors.gray['700'];
 
 const Colors = {
   colors: {
     brand: theme.colors.green,
     heading,
     text,
+    textHighlight,
   },
 };
 
