584fbe561a6157075b302424d0fdeb6637c6f959
Inzamul Haque <inzamul119@gmail.com>
2022-05-19 19:05:38 +0600

    refactor: update import and export style1

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,19 +1,17 @@
 import moment from 'moment';
-import data from '../data';
+import { festivalTime } from '../data';
 
 function getCurrentTime() {
   return moment.now();
 }
 
 function getCelebrationTime() {
-  const target = data.celebrationTime;
-  return moment(target).valueOf();
+  return moment(festivalTime).valueOf();
 }
 
 function getAvailableTime() {
   return getCelebrationTime() - getCurrentTime();
 }
 
-
-
-export default { getCurrentTime, getCelebrationTime, getAvailableTime };
+const time = { getCurrentTime, getCelebrationTime, getAvailableTime };
+export default time;
