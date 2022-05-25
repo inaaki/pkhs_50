9ea6900dde138128b2ec77c38b72f807f525aed8
Inzamul Haque <inzamul119@gmail.com>
2022-05-26 01:26:25 +0600

    fix: theme directory path update

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import Theme from './theme/_index';
+import Theme from './Theme/_index';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
