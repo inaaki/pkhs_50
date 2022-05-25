a34eb25aefbfd54e5d414d1fd8c800c8fd79e337
Inzamul Haque <inzamul119@gmail.com>
2022-05-25 23:14:42 +0600

    add primary color

--- src/Theme/foundations/color.js
+++ src/Theme/foundations/color.js
@@ -1,6 +1,7 @@
 import { theme } from '@chakra-ui/react';
 
 const heading = theme.colors.gray['600'];
+const primary = theme.colors.green['500'];
 const text = theme.colors.gray['500'];
 const textHighlight = theme.colors.gray['700'];
 
@@ -8,6 +9,7 @@ const Colors = {
   colors: {
     brand: theme.colors.green,
     heading,
+    primary,
     text,
     textHighlight,
   },
