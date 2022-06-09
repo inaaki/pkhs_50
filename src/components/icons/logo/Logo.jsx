e8420eecbfc44d5d60e71ce1ea19bb8bbda93ac9
Inzamul Haque <inzamul119@gmail.com>
2022-06-10 01:18:48 +0600

    logo: update logo design with icon image

--- src/components/icons/logo/Logo.jsx
+++ src/components/icons/logo/Logo.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import style from './Logo.module.css';
 import { Link } from 'react-router-dom';
+import fireWork from '../../../assets/images/fireworks.png';
 
 function Logo() {
   return (
     <Link to="/">
+      <img src={fireWork} className={style.image}/>
       <span className={style.main}>
         <span className={style.logo_title}>Suborno Jayanti</span>
         <span className={style.logo_year}>
