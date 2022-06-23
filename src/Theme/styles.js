25b97ac440a5ac29a04f8cde3b995d7d49aac8bb
Inzamul Haque <inzamul119@gmail.com>
2022-06-23 19:54:47 +0600

    feature: implement custom scroll design

--- src/Theme/styles.js
+++ src/Theme/styles.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-dupe-keys */
+
 const styles = {
   styles: {
     global: {
@@ -11,7 +13,28 @@ const styles = {
       },
       body: {
         color: 'text',
-        mt: '67.5px',//fixed nav height
+        //fixed nav height
+        mt: '67.5px',
+      },
+      'body::-webkit-scrollbar': {
+        //width of the entire scrollbar
+        width: '12px',
+      },
+      'body::-webkit-scrollbar-track': {
+        //color of the tracking area
+        boxShadow: 'inset 0 0 6px rgba(0,0,0,0.1)',
+        bg: '#F5F5F5',
+        borderRadius: '10px',
+      },
+      'body::-webkit-scrollbar-thumb': {
+        borderRadius: '10px',
+        border: '2px solid #f5f5f5',
+        bg: '#22543D',
+        backgroundImage:
+          '-webkit-gradient(linear, left top, left bottom, from(#276749), color-stop(#68D391), to(#276749))',
+        backgroundImage: '-o-linear-gradient(top, #276749, #68D391, #276749)',
+        backgroundImage:
+          'linear-gradient(to bottom, #276749, #68D391, #276749)',
       },
     },
   },
