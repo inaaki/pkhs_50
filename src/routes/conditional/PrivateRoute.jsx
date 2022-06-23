6da14c7e5c8dba0e371b16fa7355a7b4b585fd9e
Inzamul Haque <inzamul119@gmail.com>
2022-06-24 04:18:03 +0600

    refactor: fix user avatar broken link

--- src/routes/conditional/PrivateRoute.jsx
+++ src/routes/conditional/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import isEmpty from 'lodash/isEmpty';
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { UserContext } from '../../context/userContext';
 
