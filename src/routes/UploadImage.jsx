9c857e7670eb64fff0b36549bc33cb95c8e7d3f2
Inzamul Akib <77077129+inaaki@users.noreply.github.com>
2022-09-28 23:06:00 +0600

    feat(drag-drop): add dnd effect with react-dropzone

--- src/routes/UploadImage.jsx
+++ src/routes/UploadImage.jsx
@@ -4,6 +4,7 @@ import {
   chakra,
   Heading,
   Icon,
+  Input,
   List,
   ListIcon,
   ListItem,
@@ -11,14 +12,63 @@ import {
   VStack,
 } from '@chakra-ui/react';
 import { useState } from 'react';
+import Dropzone from 'react-dropzone';
 import { BsPlus } from 'react-icons/bs';
-import { TiTick, TiTimes } from 'react-icons/ti';
+import { TiTick } from 'react-icons/ti';
+import createToast from '../utils/toast';
+
+//TODO: implement api
 
 function UploadImage() {
-  const okIcon = <ListIcon as={TiTick} color={'green.500'} fontSize={'xl'} />;
-  const errIcon = <ListIcon as={TiTimes} color={'red.500'} fontSize={'xl'} />;
-
+  const [loading, setLoading] = useState(false);
   const [borderCol, setBorderCol] = useState('gray.400');
+  const [image, setImage] = useState('');
+  const initState = () => {
+    setBorderCol('gray.400');
+    setImage('');
+  };
+
+  const toast = createToast();
+
+  const handleDragEnter = () => setBorderCol('blue.500');
+  const handleDragLeave = () => setBorderCol(image ? 'brand.500' : 'gray.400');
+  const handleDrop = files => {
+    if (files?.length === 1) {
+      const file = files[0];
+      // check validity
+      const type = ['image/png', 'image/jpeg', 'image/webp'];
+      const MAX_SIZE = 1_000_000;
+      if (!type.includes(file.type)) {
+        toast.error('Format invalid', 'Supported format is .jpg, .jpeg, .png');
+        initState('');
+        return;
+      } else if (file.size > MAX_SIZE) {
+        toast.error('File too large', 'Maximum file size is 1MB');
+        initState('');
+        return;
+      } else {
+        const reader = new FileReader();
+        reader.onerror = () => setImage('');
+        reader.abort = () => setImage('');
+        reader.onload = ev => {
+          setImage(ev.target.result);
+        };
+        reader.readAsDataURL(file);
+        setBorderCol('brand.500');
+      }
+    } else {
+      toast.notify('Drop or select a valid photo');
+      initState('');
+    }
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+    setTimeout(() => {
+      //navigate
+      setLoading(false);
+    }, 1000);
+  };
 
   return (
     <Center mt={{ base: 28, md: 36 }} px={4}>
@@ -26,24 +76,41 @@ function UploadImage() {
         <VStack spacing={{ base: 12, md: 20 }}>
           <Heading as={'h3'}>Upload your photo:</Heading>
           <VStack spacing={8}>
-            <chakra.div
-              className="drop"
-              boxSize={'180px'}
-              border={'3px dashed'}
-              borderColor={borderCol}
-              borderRadius={'xl'}
-              cursor={'pointer'}
+            <Dropzone
+              accept={{ 'image/*': ['.jpeg', '.jpg', '.png', '.webp'] }}
+              onDragEnter={handleDragEnter}
+              onDragLeave={handleDragLeave}
+              onDrop={handleDrop}
             >
-              <VStack justifyContent={'center'} height="full">
-                <Icon as={BsPlus} boxSize={'24'} color={'gray.200'} />
-                <Text fontSize={'xs'}>Click here or drop a photo</Text>
-              </VStack>
-            </chakra.div>
+              {({ getRootProps, getInputProps }) => (
+                <chakra.div
+                  backgroundImage={image}
+                  backgroundPosition={'center'}
+                  backgroundSize={'cover'}
+                  border={`3px ${image ? 'solid' : 'dashed'}`}
+                  borderColor={borderCol}
+                  borderRadius={'xl'}
+                  boxSize={'180px'}
+                  cursor={'pointer'}
+                  overflow={'hidden'}
+                  {...getRootProps()}
+                >
+                  <Input {...getInputProps()} />
+                  {!image && (
+                    <VStack justifyContent={'center'} height="full">
+                      <Icon as={BsPlus} boxSize={'24'} color={'gray.200'} />
+                      <Text fontSize={'xs'}>Click here or drop a photo</Text>
+                    </VStack>
+                  )}
+                </chakra.div>
+              )}
+            </Dropzone>
             <Button
-              isLoading={false}
+              isLoading={loading}
               loadingText="Uploading..."
-              isDisabled={true}
+              isDisabled={!image}
               size={'sm'}
+              onClick={handleSubmit}
             >
               {' '}
               Upload Photo
@@ -53,15 +120,15 @@ function UploadImage() {
         <List color={'gray.700'} listStyleType={'none'}>
           <VStack spacing={0}>
             <ListItem>
-              {okIcon}
-              Photo must be less than
-              <chakra.code color={'red.400'}> 1MB </chakra.code>in size
+              <ListIcon as={TiTick} color={'green.500'} fontSize={'xl'} />
+              Supported size (maximum):
+              <chakra.code color={'red.400'}> 1MB </chakra.code>
             </ListItem>
             <ListItem>
-              {errIcon}
+              <ListIcon as={TiTick} color={'green.500'} fontSize={'xl'} />
               Supported format:
               <chakra.code ms={2} color={'red.400'}>
-                .jpg .jpeg .png
+                .jpg .jpeg .png .webp
               </chakra.code>
             </ListItem>
           </VStack>
